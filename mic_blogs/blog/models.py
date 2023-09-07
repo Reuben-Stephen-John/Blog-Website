71afcb107a27cc5740b12697f8e465cf691afb7e
Reuben-Stephen-John <reuben.sj2003@gmail.com>
2023-09-07 05:45:50 +0530

    better blogs

--- mic_blogs/blog/models.py
+++ mic_blogs/blog/models.py
@@ -1,11 +1,13 @@
 from django.db import models
-# Add these:
-from modelcluster.fields import ParentalKey
 
-from wagtail.models import Page, Orderable
+from wagtail.models import Page
 from wagtail.fields import RichTextField
-from wagtail.admin.panels import FieldPanel, InlinePanel
+from wagtail.admin.panels import FieldPanel
 from wagtail.search import index
+from wagtailmarkdown.fields import MarkdownField
+from wagtailmarkdown.edit_handlers import MarkdownPanel
+
+
 
 class BlogIndexPage(Page):
     intro = RichTextField(blank=True)
@@ -13,13 +15,27 @@ class BlogIndexPage(Page):
     content_panels = Page.content_panels + [
         FieldPanel('intro')
     ]
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super().get_context(request)
+        blogpages = self.get_children().live().order_by('-first_published_at')
+        context['blogpages'] = blogpages
+        return context
 
-# Keep the definition of BlogIndexPage model, and add the BlogPage model:
 
 class BlogPage(Page):
     date = models.DateField("Post date")
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
+    markdown = MarkdownField(blank=True) 
+        # Add the image field
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
 
     search_fields = Page.search_fields + [
         index.SearchField('intro'),
@@ -29,21 +45,8 @@ class BlogPage(Page):
     content_panels = Page.content_panels + [
         FieldPanel('date'),
         FieldPanel('intro'),
-        FieldPanel('body'),
-
-        # Add this:
-        InlinePanel('gallery_images', label="Gallery images"),
-    ]
-
-
-class BlogPageGalleryImage(Orderable):
-    page = ParentalKey(BlogPage, on_delete=models.CASCADE, related_name='gallery_images')
-    image = models.ForeignKey(
-        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
-    )
-    caption = models.CharField(blank=True, max_length=250)
-
-    panels = [
         FieldPanel('image'),
-        FieldPanel('caption'),
+        FieldPanel('body'),
+        MarkdownPanel('markdown'),
     ]
+
